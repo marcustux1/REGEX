592bb1fd98166c4e17689c53242f673b3b4ea465
Marcus Vinicius Arruda Carneiro <32886971+marcustux1@users.noreply.github.com>
2022-03-16 10:08:35 -0300

    Update regex_sum_1505607.py

--- Python_for_Everybody/regex_sum_1505607.py
+++ Python_for_Everybody/regex_sum_1505607.py
@@ -1,10 +1,18 @@
+# IMPORTA O MÓDULO REGEX
 import re
-text = open('Regex/regex_sum_1505607.txt')
+
+# ARMAZENA O ARQUIVO DENTRO DE UMA VARIÁVEL
+text = open('regex_sum_1505607.txt')
+
+# CRIA UMA LISTA VAZIA QUE RECEBERÁ OS NÚMEROS ENCONTRADOS NO TEXTO
 list = []
+
+# ACESSANDO CADA LINHA DO TEXTO, E BUSCANDO VIA EXPRESSÃO TEXTO QUE CASE COM O SOLICITADO
 for line in text:
     line = line.rstrip()
     numbers = re.findall('[0-9]+',line)
     for num in numbers:
         list.append(int(num))
-
+        
+# IMPRIME A LISTA DE NÚMEROS E A SOMA
 print(list,'\nThe Sum is: ', sum(list))
